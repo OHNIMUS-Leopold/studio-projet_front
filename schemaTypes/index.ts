32980af8f33ee9f9f883ba1b9f7ab7228e620947
Léopold OHNIMUS <leopold.ohnimus@edu.univ-fcomte.fr>
2024-12-09 09:17:05 +0100

    relocation des categories hors des posts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,6 @@
-import {postType, categoryType} from './postType'
+import {postType} from './postType'
+import {categoryType} from './categoryType'
 import {seo} from './blocks/seo'
+import { homepage } from './homepage'
 
-export const schemaTypes = [seo, postType, categoryType]
+export const schemaTypes = [seo, postType, categoryType, homepage]
--- schemaTypes/postType.ts
+++ schemaTypes/postType.ts
@@ -51,28 +51,3 @@ export const postType = defineType({
     }),
   ],
 })
-
-export const categoryType = defineType({
-  name: 'category',
-  title: 'Category',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      type: 'string',
-      validation: (rule) => rule.required(),
-    }),
-    defineField({
-      name: 'slug',
-      type: 'slug',
-      options: {source: 'title'},
-      validation: (rule) => rule.required(),
-    }),
-    defineField({
-      name: 'publishedAt',
-      type: 'datetime',
-      initialValue: () => new Date().toISOString(),
-      validation: (rule) => rule.required(),
-    }),
-  ],
-})
